cec084765ac51c5029f2c7af94afb8a0f332a74d
Ruby <rubykozel@gmail.com>
2019-12-28 20:35:51 +0200

    update user and recipe components

--- src/main/java/recipeapplication/components/Recipe.java
+++ src/main/java/recipeapplication/components/Recipe.java
@@ -1,8 +1,75 @@
 package recipeapplication.components;
 
+import javax.validation.constraints.NotNull;
+
 import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class Recipe {
 
+	@NotNull
+	String id;
+
+	@NotNull
+	String category;
+
+	@NotNull
+	String[] ingridiant;
+
+	@NotNull
+	String preperation;
+
+	String recipeImage; // TODO: figure out how to store images
+
+	public Recipe() {
+	}
+
+	public Recipe(String id, String category, String[] ingridiant, String preperation, String recipeImage) {
+		super();
+		this.id = id;
+		this.category = category;
+		this.ingridiant = ingridiant;
+		this.preperation = preperation;
+		this.recipeImage = recipeImage;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String[] getIngridiant() {
+		return ingridiant;
+	}
+
+	public void setIngridiant(String[] ingridiant) {
+		this.ingridiant = ingridiant;
+	}
+
+	public String getPreperation() {
+		return preperation;
+	}
+
+	public void setPreperation(String preperation) {
+		this.preperation = preperation;
+	}
+
+	public String getRecipeImage() {
+		return recipeImage;
+	}
+
+	public void setRecipeImage(String recipeImage) {
+		this.recipeImage = recipeImage;
+	}
 }
--- src/main/java/recipeapplication/components/User.java
+++ src/main/java/recipeapplication/components/User.java
@@ -1,11 +1,30 @@
 package recipeapplication.components;
 
+import javax.validation.constraints.NotNull;
+
 import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class User {
 	
+	@NotNull
 	private String id;
+	
+	@NotNull
+	private Role role;
+	
+	@NotNull
+	private Name name;
+
+	public User() {
+	}
+
+	public User(String id, Role role, Name name) {
+		super();
+		this.id = id;
+		this.role = role;
+		this.name = name;
+	}
 
 	public String getId() {
 		return id;
@@ -13,5 +32,21 @@ public class User {
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
 	}
 }
