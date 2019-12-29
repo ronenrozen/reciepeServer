5f1deefc3573328b8fa6a09b936bc20725af6fb3
Amit Goldshtein <17098942+pristit@users.noreply.github.com>
2019-12-29 20:13:32 +0200

    Added readRecipe and updateRecipe to RecipeServiceImplementation

--- src/main/java/recipeapplication/services/UserServiceImpl.java
+++ src/main/java/recipeapplication/services/UserServiceImpl.java
@@ -1,25 +1,56 @@
 package recipeapplication.services;
 
+import com.google.cloud.storage.Bucket;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import recipeapplication.components.Recipe;
+import recipeapplication.database.RecipeFirebaseCrud;
 
-import recipeapplication.components.User;
-import recipeapplication.database.UserFirebaseCrud;
+import java.io.IOException;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class RecipeServiceImpl implements RecipeService {
 
-	private UserFirebaseCrud users;
+	private RecipeFirebaseCrud recipes;
+	private Bucket bucket;
 
 	@Autowired
-	public UserServiceImpl(UserFirebaseCrud users) {
-		this.users = users;
+	public RecipeServiceImpl(RecipeFirebaseCrud recipes, Bucket bucket) {
+		this.recipes = recipes;
+		this.bucket = bucket;
 	}
 
 	@Override
-	public void addUser(User user) {
-		this.users.create(user);
+	public void addRecipe(Recipe recipe) {
+		this.recipes.create(recipe);
+	}
 
+	public void addRecipe(Recipe recipe, MultipartFile recipeImage) {
+		try {
+			if (recipeImage != null && !recipeImage.isEmpty()) {
+				String id = recipe.getId() + "." + FilenameUtils.getExtension(recipeImage.getOriginalFilename());
+				this.bucket.create(id, recipeImage.getBytes());
+				recipe.setRecipeImageId(id);
+			}
+			this.addRecipe(recipe);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public Recipe readRecipe(String recipeId) {
+		if (Integer.parseInt(recipeId) > 0)
+			return this.recipes.read(recipeId);
+		else
+			return null;
+	}
+
+	@Override
+	public void updateRecipe(Recipe recipe) {
+		this.recipes.update(recipe);
 	}
 
 }
