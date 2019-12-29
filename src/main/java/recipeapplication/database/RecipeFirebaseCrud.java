4f9dbd0926fab47268b50c1dc8c2ec1095ce40bf
Amit Goldshtein <17098942+pristit@users.noreply.github.com>
2019-12-29 20:11:29 +0200

    Added validation check to create, Added read and update (with validation checks)

--- src/main/java/recipeapplication/database/RecipeFirebaseCrud.java
+++ src/main/java/recipeapplication/database/RecipeFirebaseCrud.java
@@ -1,11 +1,15 @@
 package recipeapplication.database;
 
+import com.google.cloud.firestore.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.google.cloud.firestore.Firestore;
 import recipeapplication.components.Recipe;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Component
 public class RecipeFirebaseCrud implements FirebaseCrud<Recipe> {
@@ -22,21 +26,73 @@ public class RecipeFirebaseCrud implements FirebaseCrud<Recipe> {
 
 	@Override
 	public void create(Recipe data) {
-		
-		this.firestore.collection(collection).document(data.getId()).set(data);
-		
+		System.out.println("In recipe creation section");
+
+		try {
+			if (this.firestore.collection(collection).document(data.getId()).get().get().exists() == false)  // Check if document already exists, to not allow overwriting it with create.
+				this.firestore.collection(collection).document(data.getId()).set(data);
+			else
+				throw new Exception("Failed to create, Recipe with this ID already exists!");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@Override
-	public void read(String document) {
-		// TODO Auto-generated method stub
-		
+	public Recipe read(String document) {
+		System.out.println("In recipe reading section");
+		try {
+			DocumentSnapshot docSnap = this.firestore.collection(collection).document(document).get().get();
+			if (docSnap.exists() == false)
+				throw new Exception("Failed to read, recipe doesn't exist!");
+			else {
+				Map<String, Object> data = this.firestore.collection(collection).document(document).get().get().getData();
+
+				Recipe recipe = new Recipe();
+				recipe.setId(data.get("id").toString());
+				recipe.setCategory(data.get("category").toString());
+				recipe.setIngridiant(((List<String>) data.get("ingridiant")));
+				recipe.setPreperation(data.get("preperation").toString());
+				if (data.get("recipeImageId") != null)
+					recipe.setRecipeImageId(data.get("recipeImageId").toString());
+				return recipe;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+
 	}
 
 	@Override
 	public void update(Recipe data) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("In recipe updating section");
+
+		try {
+			DocumentSnapshot docSnap = this.firestore.collection(collection).document(data.getId()).get().get();
+			if(docSnap.exists() && docSnap.get("id").toString().equalsIgnoreCase(data.getId())) {
+				this.firestore.collection(collection).document(data.getId()).set(data);
+			}
+			else
+				throw new Exception("Failed to update, recipe doesn't exist!");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 	@Override
